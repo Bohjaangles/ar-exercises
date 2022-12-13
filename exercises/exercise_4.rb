6eb63ebf0b0a47069d6363c9d0d2c6fa61c6e624
Russel Mueller <bohjaangles@gmail.com>
2022-12-13 06:29:32 +0000

    adding more querries, including conditionals in search using active records

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@underperforming_women_store = Store.where(mens_apparel: false, womens_apparel: true, annual_revenue: ..(1000000))
+
+puts @underperforming_women_store.count()
